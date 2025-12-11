b01b51330c2393ffc4cf96df9ea2f1a8d6d5b7d9
raseward14 <raseward14@gmail.com>
2025-12-10 17:52:32 -0700

    finished custom attribute directive to enhance anchor elements

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,9 +3,21 @@ import { Directive } from '@angular/core';
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
+    host: {
+       '(click)': 'onConfirmLeavePage($event)' 
+    }
 })
 export class SafeLinkDirective {
     constructor() {
         console.log('SafeLinkDirective is active!');
     }
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantToLeave = window.confirm('Do you want to leave the app?')
+
+        if (wantToLeave) {
+            return;
+        } 
+        event?.preventDefault()
+     } 
 }
