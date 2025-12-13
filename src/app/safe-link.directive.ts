30d86a88bbaab0b9004d61ff7faca2f079da7cc3
raseward14 <raseward14@gmail.com>
2025-12-13 11:23:29 -0700

    added input to custom directive with an alias to simplify setting dynamic values

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -8,17 +8,19 @@ import { Directive } from '@angular/core';
     }
 })
 export class SafeLinkDirective {
+    queryParam = input('myApp', {alias: 'appSafeLink'});
+
     constructor() {
         console.log('SafeLinkDirective is active!');
     }
 
     onConfirmLeavePage(event: MouseEvent) {
-        const wantToLeave = window.confirm('Do you want to leave the app?')
+        const wantToLeave = window.confirm('Do you want to leave the app?');
 
         if (wantToLeave) {
             const address = (event.target as HTMLAnchorElement).href;
 
-            (event.target as HTMLAnchorElement).href = address + '?from=myApp';
+            (event.target as HTMLAnchorElement).href = address + `?from=${this.queryParam()}`;
             return;
         } 
         event?.preventDefault()
