b38fa841debdc66ff91b725f3b4210ecea0fb89d
raseward14 <raseward14@gmail.com>
2025-12-15 17:18:48 -0700

    finished AuthDirective attribute directive using authService signal input signal and effect() function to run user check every time either signal changes in auth directive

---
+++ src/app/auth/auth.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, effect, input, inject } from '@angular/core';
+
+import type { Permission } from './auth.model';
+import { AuthService } from './auth.service';
+
+@Directive({
+  selector: '[appAuth]',
+  standalone: true
+})
+export class AuthDirective {
+  userType = input.required<Permission>({ alias: 'appAuth' });
+  private authService = inject(AuthService)
+
+  constructor() {
+    effect(() => {
+      if (this.authService.activePermission() === this.userType()) {
+        console.log('SHOW ELEMENT');
+      } else {
+        console.log('HIDE ELEMENT');
+      };
+    })
+  }
+}
