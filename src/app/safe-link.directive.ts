c0f195e478fc13cd0ebe08a32f948ab948e62f4f
raseward14 <raseward14@gmail.com>
2025-12-23 15:51:06 -0700

    added logDirective to the hostDirectives decorator of the safeLinkDirective for DRY code and also imported and added to the AppComponent

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,11 +1,13 @@
 import { Directive, input, inject, ElementRef } from '@angular/core';
+import { LogDirective } from './log.directive';
 
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
     host: {
        '(click)': 'onConfirmLeavePage($event)' 
-    }
+    },
+    hostDirectives: [LogDirective]
 })
 export class SafeLinkDirective {
     queryParam = input('myApp', {alias: 'appSafeLink'});
